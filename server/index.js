95ee1b7340d76c367f8600e8d961cd61eb0907cc
Farhan Ali <fahanali3193@gmail.com>
2021-06-22 16:16:25 -0700

    Did npm install

--- server/index.js
+++ server/index.js
@@ -257,5 +257,6 @@ app.get('/qa/questions', (req, res) => {
 
 const port = 3000;
 app.listen(port, () => {
+  console.log('ENVIRONMENT', process.env.NODE_ENV)
   console.log(`Listening on port http://localhost:${port}`);
 });

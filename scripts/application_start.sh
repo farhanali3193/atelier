fdd50af66cb50652e22259f5b3a383c38874e33f
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 16:08:19 -0700

    Moved nvm dir to top in bashrc

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,3 +10,7 @@ npm run build-prod
 # pm2 reload ecosystem.config.js --env production
 # pm2 save
 # EOF
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion

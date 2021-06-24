3ad56447b56c13939decbb873de2d0a0a1a015cc
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 13:05:06 -0700

    Added appspec.yml

---
+++ scripts/application_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/ubuntu/atelier
+npm install
+npm run build-prod
+
+sudo service nginx restart
+
+cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
+pm2 reload ecosystem.config.js --env production
+pm2 save
+# EOF
---
+++ scripts/before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#copy .env, pm2 ecosystem files and move them to a temp folder
+cp /home/ubuntu/atelier/.env /home/ubuntu/temp/
+cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/
+cp /home/ubuntu/atelier/uploads /home/ubuntu/temp/
+# EOF

8e3801e9a905bc458d9ab426df63572b932ddf16
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 16:26:37 -0700

    Added nvm dir on top of app start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,6 @@
-source /home/ubuntu/.profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ubuntu/atelier
 npm install
@@ -11,6 +13,3 @@ npm run build-prod
 # pm2 save
 # EOF
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion

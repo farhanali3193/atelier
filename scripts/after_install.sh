a1a5686517ecd4f98c23bdfefe562e08994819b0
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 15:45:49 -0700

    Changed befrore and after install scripst

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,6 +4,7 @@ sudo chmod -R 777 /home/ubuntu/atelier
 
 #Copy and paste the .env file and the uploads folder into the atelier directory
 cp /home/ubuntu/temp/.env /home/ubuntu/atelier/.env
+cd /home/ubuntu/atelier
 mkdir uploads
 
 # sudo apt-get install git
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 
 #copy .env, pm2 ecosystem files and move them to a temp folder
 mkdir -p /home/ubuntu/temp/
-cp /home/ubuntu/atelier/.env /home/ubuntu/temp/
+cp /home/ubuntu/atelier/.env /home/ubuntu/temp/.env
 # cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/
 sudo rm -r -f /home/ubuntu/atelier/
 # EOF

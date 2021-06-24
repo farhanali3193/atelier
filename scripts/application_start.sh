5ec496adeea3fcdf08726444780eeb939627ffbf
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 16:54:48 -0700

    Added node path

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ubuntu/atelier
-
+sudo /home/ubuntu/.nvm/versions/node/v16.4.0/bin/node
 sudo /home/ubuntu/.nvm/versions/node/v16.4.0/bin/npm install
 # npm run build-prod
 

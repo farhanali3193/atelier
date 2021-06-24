e978887d13b687c8e215283af96e987eb6486d01
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 14:35:51 -0700

    Added copy instructions for env files

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo rm -r -f /home/ubuntu/atelier/
 
 #copy .env, pm2 ecosystem files and move them to a temp folder
-# cp /home/ubuntu/atelier/.env /home/ubuntu/temp/
-# cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/
-# cp /home/ubuntu/atelier/uploads /home/ubuntu/temp/
+cp /home/ubuntu/atelier/.env /home/ubuntu/temp/
+cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/
+cp /home/ubuntu/atelier/uploads /home/ubuntu/temp/
+sudo rm -r -f /home/ubuntu/atelier/
 # EOF

6f75fa0354399dcd4bc825d5bf50eec56a654d50
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 15:36:18 -0700

    Added after install cmds

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,7 +4,7 @@ sudo chmod -R 777 /home/ubuntu/atelier
 
 #Copy and paste the .env file and the uploads folder into the atelier directory
 cp /home/ubuntu/temp/.env /home/ubuntu/atelier/.env
-cp /home/ubuntu/temp/uploads /home/ubuntu/atelier/uploads
+mkdir uploads
 
 # sudo apt-get install git
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash

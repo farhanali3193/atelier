417ac708f14cc77ed30d9b0f2a2b1a6d6609eef5
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 14:33:13 -0700

    Please enttietir the commit message for your changes. Lines starting

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo rm -r -f /home/ubuntu/atelier/package.json
+sudo rm -r -f /home/ubuntu/atelier/README.md
 
 #copy .env, pm2 ecosystem files and move them to a temp folder
 # cp /home/ubuntu/atelier/.env /home/ubuntu/temp/

139c5d99d36481da6f68986595698746d9d64391
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 15:50:08 -0700

    Added appstart script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,7 @@ sudo chmod -R 777 /home/ubuntu/atelier
 #Copy and paste the .env file and the uploads folder into the atelier directory
 cp /home/ubuntu/temp/.env /home/ubuntu/atelier/.env
 cd /home/ubuntu/atelier
-mkdir uploads
+mkdir -p uploads
 
 # sudo apt-get install git
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,7 +6,7 @@ npm run build-prod
 
 # sudo service nginx restart
 
-cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
-pm2 reload ecosystem.config.js --env production
-pm2 save
+# cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
+# pm2 reload ecosystem.config.js --env production
+# pm2 save
 # EOF

10f8a683b045120ec145ed58e286233342997054
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 09:57:24 -0700

    Removed nginx script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 cd /home/ubuntu/atelier
 npm run build-prod
-sudo /etc/init.d/nginx reload
+# sudo /etc/init.d/nginx reload
 # cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
 # pm2 reload ecosystem.config.js --env production
 # pm2 save

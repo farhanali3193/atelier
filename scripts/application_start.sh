d5e9eca87c9df980b1614889e3e239b5286f4732
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 17:46:22 -0700

    Added source

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,8 @@
+source /home/ubuntu/.profile
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ubuntu/atelier
 npm install

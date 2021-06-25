734289fa2fe501c9a54b7d21885919ddbb5d871b
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 12:05:56 -0700

    Cleaned up scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
 #copy .env, pm2 ecosystem files and move them to a temp folder
 mkdir -p /home/ubuntu/temp/
 cp /home/ubuntu/atelier/.env /home/ubuntu/temp/.env
 cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/ecosystem.config.js
-# cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/
 cd /home/ubuntu/atelier
 pm2 kill
 sudo rm -r -f /home/ubuntu/atelier/
--- scripts/pm2_reload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PM2_HOME=/home/ubuntu/.pm2
-cd /home/ubuntu/atelier
-pm2 stop all
-# /usr/local/bin/pm2 reload ecosystem.config.js --env production

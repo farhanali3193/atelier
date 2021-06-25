f905d209880f999282c86e312181d39d66ab1229
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 22:30:58 -0700

    Added nvm dir

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,8 @@
 
 cd /home/ubuntu/atelier
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+[stdout]export NVM_DIR="$HOME/.nvm"
+[stdout][ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
 npm install
 # npm run build-prod

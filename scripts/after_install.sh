92ac27defc7f71bd1ad125a8d8c273bae315bfe9
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 00:55:40 -0700

    Added .env

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,6 @@
 sudo chmod -R 777 /home/ubuntu/atelier
 
 #Copy and paste the .env file and the uploads folder into the atelier directory
-cp /home/ubuntu/temp/.env /home/ubuntu/atelier/atelier/.env
+cp /home/ubuntu/temp/.env /home/ubuntu/atelier/.env
 cd /home/ubuntu/atelier
 # EOF

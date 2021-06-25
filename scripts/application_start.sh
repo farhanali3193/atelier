867e4d44c5fc99b724d4f921c778ffebd77c3d6b
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 02:52:10 -0700

    added ful path for ngninx

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 cd /home/ubuntu/atelier
 npm run build-prod
-sudo service nginx restart
+sudo /etc/init.d/nginx reload
 # cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
 # pm2 reload ecosystem.config.js --env production
 # pm2 save

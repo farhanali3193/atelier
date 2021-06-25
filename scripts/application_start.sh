569ad7aedba6d425e3a24579d3a14994c55f1f1d
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 01:57:55 -0700

    Using abs path

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 cd /home/ubuntu/atelier
 npm run build-prod
-pm2 stop all
+/usr/local/bin/pm2 stop all
 # /usr/local/bin/pm2 reload ecosystem.config.js --env production
 
 # cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js

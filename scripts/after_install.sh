d7934826fe1807b007169400c97768eb0f50bf4e
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 15:22:56 -0700

    Changed directory of uplaods folder to be ouside atelier

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,8 +5,8 @@ sudo chmod -R 777 /home/ubuntu/atelier
 #Copy and paste the .env file and the uploads folder into the atelier directory
 cp /home/ubuntu/temp/.env /home/ubuntu/atelier/.env
 cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
-cp -r /home/ubuntu/uploads /home/ubuntu/atelier/uploads
-sudo chmod -R 777 /home/ubuntu/atelier/uploads
+# cp -r /home/ubuntu/uploads /home/ubuntu/atelier/uploads
+# sudo chmod -R 777 /home/ubuntu/atelier/uploads
 # chown -R $USER:root /home/ubuntu/atelier/uploads
 cd /home/ubuntu/atelier
 npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 #copy .env, pm2 ecosystem files and move them to a temp folder
 mkdir -p /home/ubuntu/temp/
-mkdir -p /home/ubuntu/uploads/
+# mkdir -p /home/ubuntu/uploads/
 cp /home/ubuntu/atelier/.env /home/ubuntu/temp/.env
 cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/ecosystem.config.js
 cd /home/ubuntu/atelier

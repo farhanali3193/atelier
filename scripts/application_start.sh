06d7fb82445eadceeaf0e2adc189ac590502d5d2
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 17:41:28 -0700

    A

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo apt install nodejs npm
+
 cd /home/ubuntu/atelier
-sudo apt-get npm
 npm install
 # npm run build-prod
 

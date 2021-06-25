a3b138b0c9da5901deaded0ce39a5ea9ee95ed12
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 10:20:34 -0700

    Aded abs path pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,8 @@
 
 cd /home/ubuntu/atelier
 npm run build-prod
-sudo pm2 reload ecosystem.config.js --env production
+sudo /usr/local/bin/pm2 reload ecosystem.config.js --env production
+# sudo pm2 reload ecosystem.config.js --env production
 # sudo /etc/init.d/nginx reload
 # cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
 # pm2 reload ecosystem.config.js --env production

dda42f022f2c7d5292038336ba3d8f01d725d8c1
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 12:10:01 -0700

    Revertd to prev state

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,13 @@
 
 cd /home/ubuntu/atelier
 npm run build-prod
+# pm2 restart ecosystem.config.js --env production
+# sudo /usr/local/bin/pm2 reload ecosystem.config.js --env production
+# sudo pm2 reload ecosystem.config.js --env production
+# sudo /etc/init.d/nginx reload
+# cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
+# pm2 reload ecosystem.config.js --env production
+# pm2 save
+
 # EOF
 

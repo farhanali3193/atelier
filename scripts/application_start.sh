45f729978ab9a65fa3df420608d152e3678d9feb
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 17:42:21 -0700

    CRemoved sheabang

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm

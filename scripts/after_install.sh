e9a6462f197807a575540a78e5fe0398a6256afd
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 01:03:45 -0700

    Added ecosystems confd

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,5 +4,6 @@ sudo chmod -R 777 /home/ubuntu/atelier
 
 #Copy and paste the .env file and the uploads folder into the atelier directory
 cp /home/ubuntu/temp/.env /home/ubuntu/atelier/.env
+cp /home/ubuntu/temp/ecosystem.config.js /home/ubuntu/atelier/ecosystem.config.js
 cd /home/ubuntu/atelier
 # EOF
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,7 @@
 #copy .env, pm2 ecosystem files and move them to a temp folder
 mkdir -p /home/ubuntu/temp/
 cp /home/ubuntu/atelier/.env /home/ubuntu/temp/.env
+cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/ecosystem.config.js
 # cp /home/ubuntu/atelier/ecosystem.config.js /home/ubuntu/temp/
 sudo rm -r -f /home/ubuntu/atelier/
 # EOF

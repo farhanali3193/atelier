01bad5c01b6d470bd185b52fb3538ee55978f35d
Farhan Ali <fahanali3193@gmail.com>
2021-06-24 17:28:34 -0700

    Added symlinks in instance

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,7 @@
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ubuntu/atelier
-node server/index.js
+npm install
 # npm run build-prod
 
 # sudo service nginx restart

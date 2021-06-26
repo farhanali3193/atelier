99c57d7473e3d0bf06ed0de88ac5e9999911c5b5
Farhan Ali <fahanali3193@gmail.com>
2021-06-25 20:32:51 -0700

    Added Elastic IP

--- client/src/components/app/app.jsx
+++ client/src/components/app/app.jsx
@@ -7,7 +7,7 @@ import {findAvgRating, sortByCriteria} from '../../../../helper/reviewsHelper.js
 import ReviewsErrorBoundary from '../errorBoundary/reviewsErrorBoundary.jsx';
 import OverviewErrorBoundary from '../errorBoundary/overviewErrorBoundary.jsx'
 
-export const BACKEND_URL = process.env.NODE_ENV === 'development' ? `http://localhost:3000` : `http://3.236.115.117`;
+export const BACKEND_URL = process.env.NODE_ENV === 'development' ? `http://localhost:3000` : `http://34.225.154.204`;
 
 class App extends React.Component {
   constructor (props) {
